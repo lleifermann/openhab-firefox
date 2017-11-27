4db92821615b2ffbfd948ee1a26ef5753aa7de40
Lauritz Leifermann <lleifermann@communicode.de>
2017-11-27 20:15:33 +0100

    improved promise calls :star:

--- js/options.js
+++ js/options.js
@@ -3,14 +3,7 @@ const DEFAULT_BASICUI_PATH = "/basicui/app";
 const DEFAULT_WIDTH = 450;
 const DEFAULT_HEIGHT = 500;
 
-let localServerSync = browser.storage.sync.get('localServer');
-let remoteServerSync = browser.storage.sync.get('remoteServer');
-let basicUiSync = browser.storage.sync.get('basicUi');
-
-let widthSync = browser.storage.sync.get('width');
-let heightSync = browser.storage.sync.get('height');
-
-let remoteActiveSync = browser.storage.sync.get('remoteActive');
+let promise = browser.storage.sync.get(['localServer', 'remoteServer', 'basicUi', 'width', 'height', 'remoteActive']);
 
 function saveOptions(e) {
   browser.storage.sync.set({
@@ -24,26 +17,11 @@ function saveOptions(e) {
 }
 
 function restoreOptions() {
-  console.log("restoring options");
-
-  localServerSync.then((res) => {
+  promise.then((res) => {
     document.querySelector("#localServer").value = res.localServer || null;
-  });
-
-  remoteServerSync.then((res) => {
     document.querySelector("#remoteServer").value = res.remoteServer || DEFAULT_MYOPENHAB_URL;
-  });
-
-  basicUiSync.then((res) => {
     document.querySelector("#basicUi").value = res.basicUi || DEFAULT_BASICUI_PATH;
-  });
-
-  widthSync.then((res) => {
     document.querySelector("#width").value = res.width || DEFAULT_WIDTH;
-    console.log(res.width);
-  });
-
-  heightSync.then((res) => {
     document.querySelector("#height").value = res.height || DEFAULT_HEIGHT;
   });
 }
--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,14 @@
 document.addEventListener('DOMContentLoaded',
   function load() {
-  const basicUiSync = browser.storage.sync.get('basicUi');
-  const remoteServerSync = browser.storage.sync.get('remoteServer');
-  const localServerSync = browser.storage.sync.get('localServer');
-  const widthSync = browser.storage.sync.get('width');
-  const heightSync = browser.storage.sync.get('height');
-  const remoteActiveSync = browser.storage.sync.get('remoteActive');
+  const promise = browser.storage.sync.get(['width', 'height', 'remoteServer', 'localServer', 'remoteActive', 'basicUi']);
   let window = document.getElementById('frameContainer');
-
-    //Move into promise chain, until all variables are resolved and are ready to be injected into the iframe.
-    remoteServerSync.then((remoteServerPromise) => {
-      basicUiSync.then((basicUiPromise) => {
-        widthSync.then((widthPromise) => {
-          heightSync.then((heightPromise) => {
-            remoteActiveSync.then((remoteActivePromise) => {
-              if(remoteActivePromise.remoteActive){
-                window.innerHTML = `<iframe style="border:none;" src="${remoteServerPromise.remoteServer}${basicUiPromise.basicUi}" width="${widthPromise.width + ''}"px height="${heightPromise.height + ''}"px></iframe>`;
-              }
-              else{
-                localServerSync.then((localServerPromise) => {
-                  window.innerHTML = `<iframe style="border:none;" src="${localServerPromise.localServer}${basicUiPromise.basicUi}" width="${widthPromise.width + ''}"px height="${heightPromise.height + ''}"px></iframe>`;
-                });
-              }
-            });
-          });
-        });
-      });
+    promise.then((res) => {
+      if(res.remoteActive){
+        console.log(res);
+        window.innerHTML = `<iframe style="border:none;" src="${res.remoteServer}${res.basicUi}" width="${res.width + ''}"px height="${res.height + ''}"px></iframe>`;
+      }
+      else{
+        window.innerHTML = `<iframe style="border:none;" src="${res.localServer}${res.basicUi}" width="${res.width + ''}"px height="${res.height + ''}"px></iframe>`;
+      }
     });
-  }
-);
+});
